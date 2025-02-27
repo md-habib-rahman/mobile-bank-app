c16104afdec57a39525c9564e02e18478e9bed8a
Md. Habibur Rahman Bappy <habib.rahman@live.com>
2025-02-27 17:04:26 +0600

    login js complete

--- script/login.js
+++ script/login.js
@@ -1,1 +1,14 @@
-document.getElementById("btn-login").
+document
+  .getElementById("login-btn")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    //console.log(event);
+    const accountNumber = document.getElementById("account-number").value;
+    const pinNumber = document.getElementById("pin-number").value;
+    //console.log(accountNumber, pinNumber);
+    if (accountNumber === "01715983652" && pinNumber === "1234") {
+      window.location.href = "main.html";
+    } else {
+      alert("Account number or Pin is not correct!");
+    }
+  });
